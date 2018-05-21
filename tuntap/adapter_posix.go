b727e42bbffa8d27472a5cbc0afa4b114d3cb307
Julien Kauffmann <julien.kauffmann@freelan.org>
2018-05-21 15:11:03 -0400

    Removed RemoteIPv4 functions on OSX/linux

--- tuntap/adapter_posix.go
+++ tuntap/adapter_posix.go
@@ -7,8 +7,6 @@ import (
 	"net"
 	"runtime"
 	"syscall"
-
-	"github.com/freelan-developers/go-freelan/routing"
 )
 
 /*
@@ -18,9 +16,7 @@ import "C"
 
 type adapterImpl struct {
 	*adapterDescriptor
-	inf                   *net.Interface
-	remoteIPv4            net.IP
-	remoteIPv4RemovalFunc func()
+	inf *net.Interface
 }
 
 type adapterDescriptor struct {
@@ -91,61 +87,6 @@ func (t *adapterDescriptor) SetIPv6(addr *net.IPNet) error {
 	_, err := C.set_adapter_ipv6(t.ptr, *(*C.struct_in6_addr)(ipBytes), C.int(ones))
 
 	return err
-}
-
-func (a *adapterImpl) RemoteIPv4() (net.IP, error) {
-	if runtime.GOOS == "darwin" {
-		return a.remoteIPv4, nil
-	}
-
-	return a.adapterDescriptor.RemoteIPv4()
-}
-
-func (a *adapterImpl) SetRemoteIPv4(remoteAddr net.IP) error {
-	// tuntaposx does not support set a remote IPv4 on the tun interface.
-	//
-	// Let's add a route instead.
-	if runtime.GOOS == "darwin" {
-		// Let's get rid of the previous routing entry, if there is one.
-		if a.remoteIPv4RemovalFunc != nil {
-			a.remoteIPv4RemovalFunc()
-			a.remoteIPv4RemovalFunc = nil
-		}
-
-		ipv4, err := a.IPv4()
-
-		if err != nil {
-			return fmt.Errorf("setting remote IPv4: could not determine local IPv4 address: %s", err)
-		}
-
-		if ipv4 == nil {
-			return fmt.Errorf("setting remote IPv4: interface has no local IPv4 address")
-		}
-
-		// The remote address is contained in the network: no need to add a route for it.
-		if ipv4.Contains(remoteAddr) {
-			return nil
-		}
-
-		router := routing.NewRouter()
-		ok, err := router.AddRoute(&net.IPNet{IP: remoteAddr, Mask: net.CIDRMask(32, 32)}, ipv4.IP)
-
-		if err != nil {
-			return err
-		}
-
-		a.remoteIPv4 = remoteAddr
-
-		if ok {
-			a.remoteIPv4RemovalFunc = func() {
-				router.DeleteRoute(&net.IPNet{IP: remoteAddr, Mask: net.CIDRMask(32, 32)}, ipv4.IP)
-			}
-		}
-
-		return nil
-	}
-
-	return a.adapterDescriptor.SetRemoteIPv4(remoteAddr)
 }
 
 func newAdapter(name string, _type C.adapter_layer) (*adapterDescriptor, error) {
@@ -231,12 +172,6 @@ func NewTunAdapter(config *TunAdapterConfig) (TunAdapter, error) {
 	if config.IPv4 != nil {
 		if err = adapter.SetIPv4(config.IPv4); err != nil {
 			return nil, fmt.Errorf("setting IPv4 address to %s: %s", *config.IPv4, err)
-		}
-	}
-
-	if config.RemoteIPv4 != nil {
-		if err = adapter.SetRemoteIPv4(*config.RemoteIPv4); err != nil {
-			return nil, fmt.Errorf("setting remote IPv4 address to %s: %s", *config.RemoteIPv4, err)
 		}
 	}
 
